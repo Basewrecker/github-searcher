830673ab7a3096c8371667c0e3391f8786db9726
Basewrecker <siddhunrs@gmail.com>
2026-04-14 20:38:14 +0530

    Ouputs to a div now

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { FaGithubAlt } from "react-icons/fa";
 
 const UserSearch = () => {
   const [username, setUsername] = useState('');
   const [submittedUsername, setSubmittedUsername] = useState('');
-
-  
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['users', submittedUsername],
@@ -21,7 +20,7 @@ const UserSearch = () => {
       if (!res.ok) throw new Error('User not found');
 
       const data = await res.json();
-      console.log('data received:', data);
+      
       return data;
     },
     enabled: !!submittedUsername,
@@ -34,6 +33,7 @@ const UserSearch = () => {
   }
 
   return (
+    <>
     <form onSubmit={handleSubmit} className="form">
       <input
         type="text"
@@ -43,6 +43,23 @@ const UserSearch = () => {
       />
       <button type="submit">Submit</button>
     </form>
+
+    {isLoading && <p className="status">Loading..</p>}
+    {isError && <p className="status error">{error.message}</p>}
+
+    {data && (
+        <div className="user-card">
+            <img src={data.avatar_url} alt={data.name} className="avatar"/>
+            <h2>{data.name || data.login}</h2>
+            <p className="bio">{data.bio}</p>
+            <a href={data.html_url} className="profile-btn" target="_blank" rel="noopener noreferer">
+                <FaGithubAlt /> Github URL
+                </a>
+
+        </div>
+    )}
+    </>
+
   )
 }
 
