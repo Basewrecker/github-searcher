d64c3ad27c3cc41ef1542d7af3915d47e1c40df6
Basewrecker <siddhunrs@gmail.com>
2026-04-14 20:27:58 +0530

    Added user search ft (outputs to cli)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import UserSearch from "./components/UserSearch";
+
 const App = () => {
   return (
-    <>
-    Test
-    </>
-  )
-}
+    <div className = "container">
+      <h1>
+        Test
+      </h1>
+    <UserSearch />
+    </div>
+  );
+};
 
 export default App;
