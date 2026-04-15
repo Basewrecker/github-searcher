2fa2f22984ffc7d3a4c6361c42fdac3fce3167d1
Basewrecker <siddhunrs@gmail.com>
2026-04-15 11:12:36 +0530

    added local storage

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchGithubUser } from "../api/github";
@@ -7,7 +8,10 @@ import RecentSearches from "./RecentSearches";
 const UserSearch = () => {
   const [username, setUsername] = useState('');
   const [submittedUsername, setSubmittedUsername] = useState('');
-  const [recentUsers, setRecentUsers] = useState<string[]>([]);
+  const [recentUsers, setRecentUsers] = useState<string[]>(() => {
+    const stored = localStorage.getItem('recentUsers');
+    return stored ? JSON.parse(stored) : []
+  });
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['users', submittedUsername],
@@ -26,6 +30,10 @@ const UserSearch = () => {
         return updated.slice(0,5);
     })
   }
+
+  useEffect(() => {
+    localStorage.setItem('recentUsers',JSON.stringify(recentUsers))
+  }, [recentUsers])
 
   return (
     <>
